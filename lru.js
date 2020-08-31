6c0abbbb0d7127e64288cebc6a07256d63d35813
hisa883 <hisa1412@icloud.com>
2020-08-31 19:45:16 +0900

    putで要素の追加の処理

--- lru.js
+++ lru.js
@@ -5,15 +5,32 @@ var disc; // putとgetとq(終了)の判別
 var i = 0;
 var data = [N]; // データ
 
+class User {
+    constructor( name, main) {
+        this.name = name;
+        this.main = main;
+    }
+}
+
 
 (async function main() {
     // メインループ
     while(1){
+        // 要素の確認
+        console.log(data[i-2]);
+        console.log(data[i-1]);
+        console.log(data[i]);
+
         disc = await readUserInput('put or get or q: ');
 
         // q入力時
         if(disc == "q"){
             break;
         }
+        // put入力時の処理
+        else if(disc == "put"){
+            data[i] = new User(await readUserInput('data name: '), await readUserInput('data main: '));
+        }
+        i++;
     }
 })();
